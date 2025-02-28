d6f4c422ce7c895b1db31820c485d57c1a2d6e97
willeyzhou1 <willzhou000@gmail.com>
2025-02-28 00:03:35 -0800

    updated resume link

--- src/About/About.js
+++ src/About/About.js
@@ -11,7 +11,7 @@ export default function About() {
                 </div>
                 <img className='selfPortrait' src={hawaiiPic} alt="Me in Hawaii" width="350" height="520"/>
                 <div className='resume'>
-                    <a className='resumeLink' href='https://drive.google.com/file/d/1zthPdNsDSpeSC3SOdBg7XpnCWSuYtsXb/view?usp=sharing' target='_blank' rel="noreferrer">
+                    <a className='resumeLink' href='https://drive.google.com/file/d/1PZ-5aY1og2Ph8ahZwPn0rymZ_C8W6FU7/view?usp=sharing' target='_blank' rel="noreferrer">
                         <h2>View Resume</h2>
                     </a>
                 </div>
