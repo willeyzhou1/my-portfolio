47ce03f576da55a7adac217a5fe66be60093644a
willeyzhou1 <willzhou000@gmail.com>
2025-05-29 01:37:40 -0700

    updated links and picture

--- src/About/About.js
+++ src/About/About.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './About.css';
-import hawaiiPic from '../assets/hawaiipic.jpg'
+import elevatorPic from '../assets/elevatorpic.jpg'
 
 export default function About() {
-    const DRIVE_LINK = "https://drive.google.com/file/d/1wxUlYoA_V8h74J2_QBSWGOIR-NonXQW-/view?usp=sharing"
+    const DRIVE_LINK = "https://drive.google.com/file/d/1fztVy8pm-dNfqxu-DNBQQ2t-IEMwxziX/view?usp=sharing"
     return (
         <div id='about' className='about'>
             <div className='introduction'>
                 <div className='bigTitle'>
                     <h1>Hi! I'm Willey.</h1>
                 </div>
-                <img className='selfPortrait' src={hawaiiPic} alt="Me in Hawaii" width="350" height="520"/>
+                <img className='selfPortrait' src={elevatorPic} alt="Me in Hawaii" width="380" height="520"/>
                 <div className='resume'>
                     <a className='resumeLink' href={DRIVE_LINK} target='_blank' rel="noreferrer">
                         <h2>View Resume</h2>
--- src/NavPage/Navpage.js
+++ src/NavPage/Navpage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Navpage.css';
 
 export default function Navpage() {
