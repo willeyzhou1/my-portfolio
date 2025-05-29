f33817bb3a8dd86978c126b2b7ead4fb5652dc43
willeyzhou1 <willzhou000@gmail.com>
2025-05-28 18:23:16 -0700

    padding updates to nav bar

--- src/NavPage/Navpage.js
+++ src/NavPage/Navpage.js
@@ -5,9 +5,9 @@ export default function Navpage() {
     return (
         <div className='NavBar'>
             <nav className="nav">
-            <a href='#about'>About Me</a>
-            <a href='#projects'>My Projects</a>
-            <a href='#contact'>Contact</a>
+                <a href='#about'>About Me</a>
+                <a href='#projects'>My Projects</a>
+                <a href='#contact'>Contact</a>
             </nav>
         </div>
     );
